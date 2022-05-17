dced9517306f927e012062830fc57e381849231c
zhanghao <276001697@qq.com>
2022-05-17 16:09:35 +0800

    docs(docs): update gh-pages

--- src/await-simple.ts
+++ src/await-simple.ts
@@ -1,4 +1,8 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
-export default class DummyClass {}
+export default class DummyClass {
+  static log(v: string) {
+    console.log(v)
+  }
+}
